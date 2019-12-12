1d96069395ae0ca72db862ee5ebeb06337503684
Alec Blakeley <alecblkly@gmail.com>
2019-12-12 12:32:11 -0500

    Initial deployment env setup Co-authored-by: Austin Powell <apst12012@gmail.com> Co-authored-by: Lorenzo Simpson <lorenzosimp@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,45 @@
-// Update with your config settings.
-
 module.exports = {
+   development: {
+      client: 'sqlite3',
+      connection: {
+         filename: './database/hackathon.db3'
+      },
+      migrations: {
+         directory: './database/migrations'
+      },
+      seeds: {
+         directory: './database/seeds'
+      }
+   },
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+   staging: {
+      client: 'pg',
+      connection: {
+         database: 'my_db',
+         user: 'username',
+         password: 'password'
+      },
+      pool: {
+         min: 2,
+         max: 10
+      },
+      migrations: {
+         tableName: 'knex_migrations'
+      }
+   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+   production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+         directory: './database/migrations'
+      },
+      seed: {
+         directory: './database/seeds'
+      },
+      pool: {
+         min: 2,
+         max: 10
+      }
+   }
 };
