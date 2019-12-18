8cee97d083d1e62c40a252e450369df51a7490ce
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-18 15:09:24 -0500

    fixed app.get

--- Api/server.js
+++ Api/server.js
@@ -29,7 +29,7 @@ server.get('/',  (req, res) => {
    res.send('Server is running!');
 });
 
-app.get("/api/external", checkJwt, (req, res) => {
+server.get("/api/external", checkJwt, (req, res) => {
    res.send({
      msg: "Your Access Token was successfully validated!"
    });
