7c3052c9c1dcdab2383275491f6a770c25e71c08
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-18 13:35:42 -0500

    Added endpoints for updating hackathons, creating hackathons, and deleting a hackathon Co-authored-by: Austin Powell <apst12012@gmail.com> Co-authored-by: Alec Blakeley <alecblkly@gmail.com>

--- models/hackathons-model.js
+++ models/hackathons-model.js
@@ -1,7 +1,10 @@
 const db = require('../database/db');
 module.exports = {
     find,
-    findById
+    findById,
+    insert,
+    updateHackathon,
+    remove
 }
 
 async function find() {
@@ -9,7 +12,30 @@ async function find() {
 }
 
 async function findById(id) {
-    const hack =  await db('hackathons').where({ id }).first()
+    const hack = await db('hackathons').where({ id }).first()
     if (hack) return hack
     return -1
+}
+
+async function insert(hackathon) {
+    return await db('hackathons').insert(hackathon, 'id')
+        .then(ids => {
+            const [id] = ids
+            return findById(id)
+        })
+}
+
+async function updateHackathon(id, changes) {
+    await db('hackathons')
+        .where({ id })
+        .update(changes)
+    return db('hackathons')
+        .where({ id })
+        .first()
+}
+
+async function remove(id) {
+    return db('hackathons')
+        .where({ id })
+        .del();
 }
