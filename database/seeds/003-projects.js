71f451e831c23ae4fcfdd99d67eb70a1de2c3fcb
Alec Blakeley <53489786+alecblkly@users.noreply.github.com>
2020-01-22 12:00:39 -0500

    Update 003-projects.js
    Changed .truncate() to .del() for PG on Heroku

--- database/seeds/003-projects.js
+++ database/seeds/003-projects.js
@@ -1,7 +1,7 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('projects').truncate()
+  return knex('projects').del()
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
