962114514a2af0c821c56f63384acdbb9c89c2ed
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-01-23 11:48:53 -0500

    changes all to restrict

--- database/migrations/20200122183804_002-hackathons.js
+++ database/migrations/20200122183804_002-hackathons.js
@@ -12,8 +12,8 @@ exports.up = function(knex) {
        tbl.integer('organizer_id')
           .references('id')
           .inTable('users')
-          .onUpdate('CASCADE')
-          .onDelete('CASCADE');
+          .onUpdate('RESTRICT')
+          .onDelete('RESTRICT');
     });
  };
  
