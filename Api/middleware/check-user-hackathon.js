338e107bce3b6ff78a7c89d51afaba4f57a0f523
Alec Blakeley <alecblkly@gmail.com>
2020-01-28 13:44:45 -0500

    Updated middleware for joining a hackathon, separate error messages. Co-Authored-By: Lorenzo Simpson <lorenzosimp@gmail.com>

--- Api/middleware/check-user-hackathon.js
+++ Api/middleware/check-user-hackathon.js
@@ -1,8 +1,17 @@
-const userHackathon = require('../../models/user_hackathons-model')
+const userHackathon = require('../../models/user_hackathons-model');
 
 module.exports = async (req, res, next) => {
-    const {user_id, id} = req.params
-    const hackathon_id = id
-    const registered = await userHackathon.findRegistered(hackathon_id, user_id)
-    registered.length ? res.status(401).json({ error: "You've already signed up for this hackathon"}) : next()
-}
+   const { user_id, id } = req.params;
+   const hackathon_id = id;
+   const registered = await userHackathon.findRegistered(hackathon_id, user_id);
+   registered.length
+      ? !req.body.developer_role
+         ? res.status(401).json({
+              error:
+                 "The user you're trying to register is already associated with this hackathon."
+           })
+         : res
+              .status(401)
+              .json({ error: "You've already signed up for this hackathon." })
+      : next();
+};
