29b6cbdd6baffd088bb7e8b0c078f1f0e3d2ce03
Michael <skaman4459@gmail.com>
2020-03-23 16:59:44 -0400

    backend for project judging mostly setup

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,11 +3,13 @@ const userRouter = require('./users-router')
 const hackathonRouter = require('./hackathons-router')
 const projectsRouter = require('./project-router')
 const projectSubmission = require('./project-submission-router')
+const hackathonJudges = require('./hackathon-judges-router');
 
 router.use('/users', userRouter);
 router.use('/hackathons', hackathonRouter);
-router.use('/projects', projectsRouter)
-router.use('/project-submission', projectSubmission)
+router.use('/projects', projectsRouter);
+router.use('/project-submission', projectSubmission);
+router.use('/hackathon-judges', hackathonJudges);
 
 module.exports = router;
 
